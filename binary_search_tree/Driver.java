e403e7df4d53f3aeced143fe7d65d3f911c489c5
Miko Nunez <nunyezmiko@gmail.com>
2021-11-16 21:16:32 +0800

    added code to insert nodes in tree and view nodes in order

--- binary_search_tree/Driver.java
+++ binary_search_tree/Driver.java
@@ -1,5 +1,14 @@
 public class Driver {
     public static void main(String[] args) {
-        
+        BST tree = new BST(); //constructor
+
+        // insert nodes
+        tree.insert(10);
+        tree.insert(20);
+        tree.insert(30);
+        tree.insert(40);
+        tree.insert(50);
+
+        tree.inorder();
     }
 }
